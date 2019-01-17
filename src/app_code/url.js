1e6b137e5a6ff48d6335fb8191fe35c4b00aab9c
Lius Patt <liuspatt@MacBook-Pro-de-Lius.local>
2019-01-17 09:08:37 -0500

    add 404 page

--- src/app_code/url.js
+++ src/app_code/url.js
@@ -4,7 +4,7 @@
 
 import React from 'react'
 
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter, Route ,Switch} from "react-router-dom";
 
 // ===================
 // Import pages 
@@ -15,18 +15,26 @@ import Redux_data from './controllers/redux_data';
 import Other_url from './controllers/other_url';
 
 
+const Page404 = ({ location }) => (
+    <div>
+       <h2>No match found for <code>{location.pathname}</code></h2>
+    </div>
+ );
+
+
 // ===================
 // Create router
 // ===================
 
 const AppRouter = () => (
-    <Router>
-        <div>
+    <BrowserRouter>
+        <Switch>
             <Route path="/" exact component={List}/>
             <Route path="/redux/" component={Redux_data}/>
             <Route path="/other_url/" component={Other_url}/>
-        </div>
-    </Router>
+            <Route component={Page404} />
+        </Switch>
+    </BrowserRouter>
 );
 
 export default AppRouter;
